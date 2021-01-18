c1c82aa37a20b88149dcbfda843d89b6a7c2af8a
Ormannishe <james.roose@hotmail.com>
2021-01-18 18:41:14 -0500

    Initial Commit

---
+++ WoWGoldGambler.lua
@@ -0,0 +1,14 @@
+WoWGoldGambler = LibStub("AceAddon-3.0"):NewAddon("WoWGoldGambler", "AceConsole-3.0")
+
+function WoWGoldGambler:OnInitialize()
+    -- Called when the addon is loaded
+    self:Print("Hello World!")
+end
+
+function WoWGoldGambler:OnEnable()
+    -- Called when the addon is enabled
+end
+
+function WoWGoldGambler:OnDisable()
+    -- Called when the addon is disabled
+end
